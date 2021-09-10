f531f7d2356baea09daabbee99ea8f864642b8e2
HeetShah04 <heet9417@gmail.com>
2021-09-10 11:53:41 +0530

    attendance report controller

--- Ashirvad-main/Ashirvad.Web/Controllers/AttendanceReportController.cs
+++ Ashirvad-main/Ashirvad.Web/Controllers/AttendanceReportController.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using Ashirvad.ServiceAPI.ServiceAPI.Area.Attendance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Ashirvad.Web.Controllers
 {
-    public class AttendanceReportController : Controller
+    public class AttendanceReportController : BaseController
     {
+
+        private readonly IAttendanceService _attendanceService = null;
         // GET: AttendanceReport
+
+        public AttendanceReportController(IAttendanceService attendanceService)
+        {
+            _attendanceService = attendanceService;
+        }
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SaveReport(DateTime FromDate, DateTime ToDate, long BranchId, long StandardId, int BatchTime)
+        {
+            var data = await this._attendanceService.GetAllAttendanceByFilter(FromDate, ToDate,BranchId,StandardId,BatchTime);
+            return View("~/Views/AttendanceReport/Manage.cshtml", data.Data);
         }
     }
 }
