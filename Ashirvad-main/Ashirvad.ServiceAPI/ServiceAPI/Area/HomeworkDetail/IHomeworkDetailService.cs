c622e35618478f99c2e82aeae323f493d3603323
jeet1210 <61597949+jeet1210@users.noreply.github.com>
2021-10-15 17:49:33 +0530

    Home Details

---
+++ Ashirvad-main/Ashirvad.ServiceAPI/ServiceAPI/Area/HomeworkDetail/IHomeworkDetailService.cs
@@ -0,0 +1,20 @@
+﻿using Ashirvad.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashirvad.ServiceAPI.ServiceAPI.Area
+{
+    public interface IHomeworkDetailService
+    {
+        Task<HomeworkDetailEntity> HomeworkdetailMaintenance(HomeworkDetailEntity homeworkDetail);
+        Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailByHomeWork(long HomeworkID);
+        Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailWithoutContentByHomeWork(long HomeworkID);
+        Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailByHomeWorkStudentID(long HomeworkID, long studentID);
+        Task<HomeworkDetailEntity> GetHomeworkByID(long HomeWorkDetailID);
+        bool RemoveHomeWork(long HomeWorkDetailID, string lastupdatedby);
+
+    }
+}
---
+++ Ashirvad-main/Ashirvad.ServiceAPI/Services/Area/HomeworkDetail/HomeworkDetailService.cs
@@ -0,0 +1,130 @@
+﻿using Ashirvad.Data;
+using Ashirvad.Logger;
+using Ashirvad.Repo.DataAcceessAPI.Area;
+using Ashirvad.ServiceAPI.ServiceAPI.Area;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashirvad.ServiceAPI.Services.Area
+{
+    public class HomeworkDetailService : IHomeworkDetailService
+    {
+        private readonly IHomeworkDetailsAPI _testContext;
+        public HomeworkDetailService(IHomeworkDetailsAPI testContext)
+        {
+            _testContext = testContext;
+        }
+
+        public async Task<HomeworkDetailEntity> HomeworkdetailMaintenance(HomeworkDetailEntity homeworkDetail)
+        {
+            HomeworkDetailEntity homeworkDetailEntity = new HomeworkDetailEntity();
+            try
+            {
+                if (homeworkDetail.FileInfo != null)
+                {
+                    homeworkDetail.AnswerSheetContent = Common.Common.ReadFully(homeworkDetail.FileInfo.InputStream);
+                    homeworkDetail.AnswerSheetName = Path.GetFileName(homeworkDetail.FileInfo.FileName);
+                }
+                else
+                {
+                    homeworkDetail.AnswerSheetContent = Convert.FromBase64String(homeworkDetail.AnswerSheetContentText);
+                }
+                var data = await _testContext.HomeworkMaintenance(homeworkDetail);
+                if (data > 0)
+                {
+                    if (!string.IsNullOrEmpty(Common.Common.GetStringConfigKey("DocDirectory")))
+                    {
+                        Common.Common.SaveFile(homeworkDetail.AnswerSheetContent, homeworkDetail.AnswerSheetName, "StudentAnswerSheet\\");
+                    }
+                }          
+                return homeworkDetailEntity;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return homeworkDetailEntity;
+        }
+
+        public async Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailByHomeWork(long HomeworkID)
+        {
+            try
+            {
+                var data = await _testContext.GetAllHomeworkByStudent(HomeworkID);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return null;
+        }
+
+        public async Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailWithoutContentByHomeWork(long HomeworkID)
+        {
+            try
+            {
+                var data = await _testContext.GetAllHomeworkdetailWithoutContentByStudentID(HomeworkID);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return null;
+        }
+
+        public async Task<List<HomeworkDetailEntity>> GetAllHomeworkdetailByHomeWorkStudentID(long HomeworkID, long studentID)
+        {
+            try
+            {
+                var data = await _testContext.GetAllAnsSheetByStudentID(HomeworkID, studentID);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return null;
+        }
+
+        public async Task<HomeworkDetailEntity> GetHomeworkByID(long HomeWorkDetailID)
+        {
+            try
+            {
+                var data = await _testContext.GetHomeworkByHomeworkID(HomeWorkDetailID);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return null;
+        }
+
+        public bool RemoveHomeWork(long HomeWorkDetailID, string lastupdatedby)
+        {
+            try
+            {
+                var data = _testContext.RemoveHomework(HomeWorkDetailID, lastupdatedby);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                EventLogger.WriteEvent(Logger.Severity.Error, ex);
+            }
+
+            return false;
+        }
+  
+    }
+}
