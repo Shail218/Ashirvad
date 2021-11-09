5d6bb9f7767d7a8965d30ae2fb10f0efc897b831
HeetShah04 <83007509+ShailUniqtech@users.noreply.github.com>
2021-11-09 15:37:03 +0530

    Changes in Branch Course

---
+++ Ashirvad-main/Ashirvad.Data/BranchCourseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashirvad.Data
+{
+    public class BranchCourseEntity
+    {
+        public long course_dtl_id { get; set; }
+        public BranchEntity branch { get; set; }
+        public CourseEntity course { get; set; }
+        public TransactionEntity Transaction { get; set; }
+        public RowStatusEntity RowStatus { get; set; }
+        public bool iscourse { get; set; }
+    }
+}
