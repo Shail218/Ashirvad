469104179f63f885120c7a82c29f0da0188987e6
jainam69 <jainamdoshi45@gmail.com>
2021-11-18 17:27:37 +0530

    changes

---
+++ Ashirvad-main/Ashirvad.API/Controllers/BranchClassController.cs
@@ -0,0 +1,93 @@
+﻿using Ashirvad.Data;
+using Ashirvad.Data.Model;
+using Ashirvad.ServiceAPI.ServiceAPI.Area;
+using Ashirvad.ServiceAPI.ServiceAPI.Area.Class;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Ashirvad.Web.Controllers
+{
+    [RoutePrefix("api/branchclass/v1")]
+    public class BranchClassController : ApiController
+    {
+        private readonly IClassService _ClassService;
+        private readonly IBranchClassService _branchClassService;
+        public BranchClassController(IClassService ClassService, IBranchClassService branchClassService)
+        {
+            _ClassService = ClassService;
+            _branchClassService = branchClassService;
+        }
+
+        [Route("BranchClassMaintenance")]
+        [HttpPost]
+        public OperationResult<BranchClassEntity> BranchClassMaintenance(BranchClassEntity branchClassEntity)
+        {
+            var data = this._branchClassService.BranchClassMaintenance(branchClassEntity);
+            OperationResult<BranchClassEntity> result = new OperationResult<BranchClassEntity>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetBranchClassByBranchClassID")]
+        [HttpPost]
+        public OperationResult<List<BranchClassEntity>> GetBranchClassByBranchClassID(long ClassID, long branchID)
+        {
+            var data = this._branchClassService.GetBranchClassByBranchClassID(ClassID, branchID);
+            OperationResult<List<BranchClassEntity>> result = new OperationResult<List<BranchClassEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetAllBranchClass")]
+        [HttpPost]
+        public OperationResult<List<BranchClassEntity>> GetAllBranchClass(long branchID)
+        {
+            var data = this._branchClassService.GetAllBranchClass(branchID);
+            OperationResult<List<BranchClassEntity>> result = new OperationResult<List<BranchClassEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetAllClass")]
+        [HttpGet]
+        public OperationResult<List<ClassEntity>> GetAllClass()
+        {
+            var data = this._ClassService.GetAllClass();
+            OperationResult<List<ClassEntity>> result = new OperationResult<List<ClassEntity>>();
+            result.Completed = true;
+            result.Data = data.Result.Data;
+            return result;
+        }
+
+        [Route("RemoveClassDetail")]
+        [HttpPost]
+        public OperationResult<bool> RemoveClassDetail(long ClassID, long BranchID, string lastupdatedby)
+        {
+            var data = _branchClassService.RemoveBranchClass(ClassID, BranchID, lastupdatedby);
+            OperationResult<bool> result = new OperationResult<bool>();
+            result.Completed = true;
+            result.Data = data;
+            return result;
+        }
+
+        [Route("GetAllBranchClass")]
+        [HttpPost]
+        public OperationResult<List<BranchClassEntity>> GetAllBranchClass(long BranchID, long CourseID)
+        {
+            var data = _branchClassService.GetAllBranchClass(BranchID, CourseID);
+            OperationResult<List<BranchClassEntity>> result = new OperationResult<List<BranchClassEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+    }
+}
---
+++ Ashirvad-main/Ashirvad.API/Controllers/BranchCourseController.cs
@@ -0,0 +1,94 @@
+﻿using Ashirvad.Common;
+using Ashirvad.Data;
+using Ashirvad.Data.Model;
+using Ashirvad.ServiceAPI.ServiceAPI.Area;
+using Ashirvad.ServiceAPI.ServiceAPI.Area.Class;
+using Ashirvad.ServiceAPI.ServiceAPI.Area.Course;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Ashirvad.Web.Controllers
+{
+    [RoutePrefix("api/branchcource/v1")]
+    public class BranchCourseController : ApiController
+    {
+        private readonly ICourseService _courseService;
+        private readonly IBranchCourseService _branchcourseService;
+        public BranchCourseController(ICourseService courseService, IBranchCourseService branchCourseService)
+        {
+            _courseService = courseService;
+            _branchcourseService = branchCourseService;
+        }
+
+        [Route("BranchCourseMaintenance")]
+        [HttpPost]
+        public OperationResult<BranchCourseEntity> BranchCourseMaintenance(BranchCourseEntity branchClassEntity)
+        {
+            var data = this._branchcourseService.BranchCourseMaintenance(branchClassEntity);
+            OperationResult<BranchCourseEntity> result = new OperationResult<BranchCourseEntity>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetBranchCourseByBranchCourseID")]
+        [HttpPost]
+        public OperationResult<List<BranchCourseEntity>> GetBranchCourseByBranchCourseID(long ClassID)
+        {
+            var data = this._branchcourseService.GetBranchCourseByBranchCourseID(ClassID);
+            OperationResult<List<BranchCourseEntity>> result = new OperationResult<List<BranchCourseEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetAllBranchCourse")]
+        [HttpPost]
+        public OperationResult<List<BranchCourseEntity>> GetAllBranchCourse(long branchID)
+        {
+            var data = this._branchcourseService.GetAllBranchCourse(branchID);
+            OperationResult<List<BranchCourseEntity>> result = new OperationResult<List<BranchCourseEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+
+        [Route("GetAllCourse")]
+        [HttpGet]
+        public OperationResult<List<CourseEntity>> GetAllCourse()
+        {
+            var data = this._courseService.GetAllCourse();
+            OperationResult<List<CourseEntity>> result = new OperationResult<List<CourseEntity>>();
+            result.Completed = true;
+            result.Data = data.Result.Data;
+            return result;
+        }
+
+        [Route("RemoveClassDetail")]
+        [HttpPost]
+        public OperationResult<bool> RemoveClassDetail(long BranchCourseID, string lastupdatedby)
+        {
+            var data = _branchcourseService.RemoveBranchCourse(BranchCourseID, lastupdatedby);
+            OperationResult<bool> result = new OperationResult<bool>();
+            result.Completed = true;
+            result.Data = data;
+            return result;
+        }
+
+        [Route("GetAllBranchCourse")]
+        [HttpPost]
+        public OperationResult<List<BranchCourseEntity>> GetAllBranchCourse(long BranchID, long CourseID)
+        {
+            var data = _branchcourseService.GetAllBranchCourse(BranchID);
+            OperationResult<List<BranchCourseEntity>> result = new OperationResult<List<BranchCourseEntity>>();
+            result.Completed = true;
+            result.Data = data.Result;
+            return result;
+        }
+    }
+}
