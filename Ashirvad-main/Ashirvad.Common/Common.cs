df529a260e8254ff294b81e2d855db59084d944a
jeet1210 <61597949+jeet1210@users.noreply.github.com>
2021-12-23 12:58:57 +0530

    Test

--- Ashirvad-main/Ashirvad.Common/Common.cs
+++ Ashirvad-main/Ashirvad.Common/Common.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -73,6 +74,75 @@ namespace Ashirvad.Common
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public class DataTableAjaxPostModel
+        {
+            // properties are not capital due to json mapping
+            public int draw { get; set; }
+            public int start { get; set; }
+            public int length { get; set; }
+            public List<Column> columns { get; set; }
+            public Search search { get; set; }
+            public List<Order> order { get; set; }
+            public Order orderdata { get; set; }
+        }
+
+        public class Column
+        {
+            public string data { get; set; }
+            public string name { get; set; }
+            public bool searchable { get; set; }
+            public bool orderable { get; set; }
+            public Search search { get; set; }
+        }
+
+        public class Search
+        {
+            public string value { get; set; }
+            public string regex { get; set; }
+        }
+
+        public class Order
+        {
+            public int column { get; set; }
+            public string name { get; set; }
+            public string dir { get; set; }
+        }
+
+        public class JqueryDatatableParam
+        {
+            public string sEcho { get; set; }
+            public string sSearch { get; set; }
+            public int iDisplayLength { get; set; }
+            public int iDisplayStart { get; set; }
+            public int iColumns { get; set; }
+            public int iSortCol_0 { get; set; }
+            public string sSortDir_0 { get; set; }
+            public int iSortingCols { get; set; }
+            public string sColumns { get; set; }
+        }
+
+
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string columnName, bool isAscending = true)
+        {
+            if (String.IsNullOrEmpty(columnName))
+            {
+                return source;
+            }
+
+            ParameterExpression parameter = Expression.Parameter(source.ElementType, "");
+
+            MemberExpression property = Expression.Property(parameter, columnName);
+            LambdaExpression lambda = Expression.Lambda(property, parameter);
+
+            string methodName = isAscending ? "OrderBy" : "OrderByDescending";
+
+            Expression methodCallExpression = Expression.Call(typeof(Queryable), methodName,
+                                  new Type[] { source.ElementType, property.Type },
+                                  source.Expression, Expression.Quote(lambda));
+
+            return source.Provider.CreateQuery<T>(methodCallExpression);
         }
     }
 }
