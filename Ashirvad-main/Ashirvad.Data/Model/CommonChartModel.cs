6dc471c57202ebc441f9f943b3133f40a83ecb7a
HeetShah04 <83007509+ShailUniqtech@users.noreply.github.com>
2022-01-06 15:38:37 +0530

    Add Commonchartmodel

--- Ashirvad-main/Ashirvad.Data/Model/CommonChartModel.cs
+++ Ashirvad-main/Ashirvad.Data/Model/CommonChartModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace Ashirvad.Data.Model
 {
     public class CommonChartModel
     {
+        List<ChartBranchEntity> branchlist { get; set; } = new List<ChartBranchEntity>();
+        List<BranchStandardEntity> branchstandardlist { get; set; } = new List<BranchStandardEntity>();
+    }
+    public class ChartBranchEntity
+    {
+        public int y { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public string drilldown { get; set; }
+        public long branchid { get; set; }
+    }
+
+    public class BranchStandardEntity
+    {
+        public string id { get; set; }
+        public long branchid { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public string standardname { get; set; }
+        public ArrayList data = new ArrayList();
     }
 }
