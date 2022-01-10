ff3ae58f378c1d61d7b742d2c8301885076a70fc
HeetShah04 <83007509+ShailUniqtech@users.noreply.github.com>
2022-01-10 13:35:44 +0530

    Changes

--- Ashirvad-main/Ashirvad.Repo/DataAcceessAPI/Area/DashboardChart/IDashboardChartAPI.cs
+++ Ashirvad-main/Ashirvad.Repo/DataAcceessAPI/Area/DashboardChart/IDashboardChartAPI.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Ashirvad.Common.Common;
 
 namespace Ashirvad.Repo.DataAcceessAPI.Area.DashboardChart
 {
@@ -16,7 +17,7 @@ namespace Ashirvad.Repo.DataAcceessAPI.Area.DashboardChart
         Task<List<ChartBranchEntity>> GetTotalCountList(long studentid);
         Task<List<DataPoints>> GetHomeworkByStudent(long branchid, long studentid);
         Task<List<TestDataPoints>> GetTestdetailsByStudent(long branchid, long studentid);
-        Task<List<MarksEntity>> GetTestDetailsByStudent(long studentid, long subjectid);
-        Task<List<HomeworkDetailEntity>> GetHomeworkDetailsByStudent(long studentid, long subjectid);
+        Task<List<MarksEntity>> GetTestDetailsByStudent(DataTableAjaxPostModel model,long studentid, long subjectid);
+        Task<List<HomeworkDetailEntity>> GetHomeworkDetailsByStudent(DataTableAjaxPostModel model,long studentid, long subjectid);
     }
 }
