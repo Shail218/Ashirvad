f5eeca7dd8179ab05ffec165b678a45dd8814761
HeetShah04 <heet9417@gmail.com>
2022-03-04 17:54:11 +0530

    Notification for Class and Course Repo and Service Side - Heet

--- Ashirvad-main/Ashirvad.Repo/DataAcceessAPI/Area/Notification/INotificationAPI.cs
+++ Ashirvad-main/Ashirvad.Repo/DataAcceessAPI/Area/Notification/INotificationAPI.cs
@@ -17,6 +17,6 @@ namespace Ashirvad.Repo.DataAcceessAPI.Area.Notification
         Task<List<NotificationEntity>> GetAllNotification(long branchID, int typeID);
         Task<List<NotificationEntity>> GetAllCustomNotification(DataTableAjaxPostModel model, long branchID, int typeID);
         Task<List<NotificationEntity>> GetAllNotificationforexcel(long branchID);
-
+        Task<List<NotificationEntity>> GetMobileNotification(long branchID, int typeID);
     }
 }
--- Ashirvad-main/Ashirvad.Repo/Services/Area/Notification/Notification.cs
+++ Ashirvad-main/Ashirvad.Repo/Services/Area/Notification/Notification.cs
@@ -47,6 +47,7 @@ namespace Ashirvad.Repo.Services.Area.Notification
             {
                 var notifID = notificationMaster.notif_id;
                 var result = await this.AddNotificationType(notificationInfo.NotificationType, notifID);
+                NotificationStandardMaintenance(notificationInfo);
                 NotifyList(notificationInfo);
                 return notifID;
             }
@@ -128,6 +129,56 @@ namespace Ashirvad.Repo.Services.Area.Notification
                             NotificationID = u.notif_id,
                             Notification_Date = u.notification_date,
                             Branch = new BranchEntity() { BranchID = branch != null ? branch.branch_id : 0, BranchName = branch != null ? branch.branch_name : "All Branch" },
+                            Transaction = new TransactionEntity() { TransactionId = u.trans_id }
+                        }).Distinct().OrderByDescending(a => a.NotificationID).ToList();
+
+            if (data?.Count > 0)
+            {
+                foreach (var item in data)
+                {
+                    int idx = data.IndexOf(item);
+                    data[idx].NotificationType = this.context.NOTIFICATION_TYPE_REL.Where(z => z.notif_id == item.NotificationID).Select(y => new NotificationTypeEntity() { ID = y.unique_id, TypeID = y.sub_type_id, TypeText = y.sub_type_id == 1 ? "Admin" : y.sub_type_id == 2 ? "Teacher" : "Student" }).ToList();
+                }
+            }
+
+            return data;
+        }
+        public async Task<List<NotificationEntity>> GetMobileNotification(long branchID, int typeID)
+        {
+            var data = (from u in this.context.NOTIFICATION_MASTER
+                        join t in this.context.NOTIFICATION_TYPE_REL on u.notif_id equals t.notif_id
+                        join b in this.context.BRANCH_MASTER on u.branch_id equals b.branch_id into tempBranch
+                        from branch in tempBranch.DefaultIfEmpty()
+                        where (branchID == 0 || u.branch_id == 0 || u.branch_id.Value == branchID)
+                        && (0 == typeID || t.sub_type_id == typeID) && u.row_sta_cd == 1
+                        select new NotificationEntity()
+                        {
+                            RowStatus = new RowStatusEntity()
+                            {
+                                RowStatus = u.row_sta_cd == 1 ? Enums.RowStatus.Active : Enums.RowStatus.Inactive,
+                                RowStatusId = (int)u.row_sta_cd
+                            },
+                            NotificationMessage = u.notif_message,
+                            NotificationID = u.notif_id,
+                            Notification_Date = u.notification_date,
+                            Branch = new BranchEntity() { BranchID = branch != null ? branch.branch_id : 0, BranchName = branch != null ? branch.branch_name : "All Branch" },
+                            list = (from b in this.context.NOTIFICATION_STD_MASTER
+                                    where  b.notif_id == u.notif_id
+                                    select new NotificationStandardEntity()
+                                    {
+                                        notif_id = b.notif_id,
+                                        BranchCourse = new BranchCourseEntity()
+                                        {
+                                            course_dtl_id = b.course_dtl_id.HasValue ? b.course_dtl_id.Value : 0,
+                                            course = new CourseEntity()
+                                            {
+                                                CourseID = b.COURSE_DTL_MASTER.COURSE_MASTER.course_id,
+                                                CourseName = b.COURSE_DTL_MASTER.COURSE_MASTER.course_name
+                                            }
+                                        },
+                                        std_id = b.class_dtl_id.HasValue ? b.class_dtl_id.Value : 0,
+                                        standard = b.CLASS_DTL_MASTER.CLASS_MASTER.class_name
+                                    }).Distinct().ToList(),
                             Transaction = new TransactionEntity() { TransactionId = u.trans_id }
                         }).Distinct().OrderByDescending(a => a.NotificationID).ToList();
 
@@ -291,7 +342,7 @@ namespace Ashirvad.Repo.Services.Area.Notification
             try
             {
                 List<string> da = new List<string>();
-                foreach(var z in notification.NotificationType)
+                foreach (var z in notification.NotificationType)
                 {
                     da.Add(z.TypeID.ToString());
                 }
@@ -303,41 +354,64 @@ namespace Ashirvad.Repo.Services.Area.Notification
                     {
                         foreach (var i in admin)
                         {
-                           
-                            sendNotification(i.fcm_token,notification.NotificationMessage,notification.Notification_Date.ToString("dd/MM/yyyy"));
+
+                            sendNotification(i.fcm_token, notification.NotificationMessage, notification.Notification_Date.ToString("dd/MM/yyyy"));
                         }
                     }
-                    
-                }
-                if ( da.Contains("2"))
-                {
-                    var teacher = this.context.USER_DEF.Where(x => x.fcm_token != null && x.branch_id == notification.Branch.BranchID && x.row_sta_cd == 1 && x.user_type == (int)Enums.UserType.Staff).Select(x => new { x.fcm_token }).ToList();
 
-                    if (teacher?.Count > 0)
-                    {
-                        foreach (var i in teacher)
-                        {
+                }
+                /*if ( da.Contains("2"))
+                //{
+                //    var teacher = this.context.USER_DEF.Where(x => x.fcm_token != null && x.branch_id == notification.Branch.BranchID && x.row_sta_cd == 1 && x.user_type == (int)Enums.UserType.Staff).Select(x => new { x.fcm_token }).ToList();
+                //    var te = (from x in this.context.USER_DEF
+                //              where x.fcm_token != null 
+                //              && x.branch_id == notification.Branch.BranchID
+                //              && x.row_sta_cd == 1
+                //              && x.user_type == (int)Enums.UserType.Staff
+                //              select x).ToList();
+                //    //foreach(var c in te)
+                //    //{
+                //    //    foreach(var v in notification.Standardlist)
+                //    //    var contact = this.context.FA.Where(x=>x.staff_id == c.staff_id && x.s == v.StandardID)
+                //    //}
+                //    if (teacher?.Count > 0)
+                //    {
+                //        foreach (var i in teacher)
+                //        {
                             
-                            sendNotification(i.fcm_token, notification.NotificationMessage, notification.Notification_Date.ToString("dd/MM/yyyy"));
-                        }
-                    }
+                //            sendNotification(i.fcm_token, notification.NotificationMessage, notification.Notification_Date.ToString("dd/MM/yyyy"));
+                //        }
+                //    }
                    
-                }
-                if ( da.Contains("3"))
+                }*/
+                if (da.Contains("3"))
                 {
-                    var student = this.context.USER_DEF.Where(x => x.fcm_token != null && x.branch_id == notification.Branch.BranchID && x.row_sta_cd == 1 && (x.user_type == (int)Enums.UserType.Student || x.user_type == (int)Enums.UserType.Parent)).Select(x => new { x.fcm_token }).ToList();
-
-                    if (student?.Count > 0 ) {
-                        foreach (var i in student)
+                    var student = this.context.USER_DEF.Where(x => x.fcm_token != null && x.branch_id == notification.Branch.BranchID && x.row_sta_cd == 1 && (x.user_type == (int)Enums.UserType.Student || x.user_type == (int)Enums.UserType.Parent)).Select(x => new { x.fcm_token, x.student_id }).ToList();
+                    foreach (var z in notification.Standardlist)
+                    {
+                        if (z != null)
                         {
-                          
-                            sendNotification(i.fcm_token, notification.NotificationMessage, notification.Notification_Date.ToString("dd/MM/yyyy"));
+                            foreach (var c in student)
+                            {
+                                var st = this.context.STUDENT_MASTER.Where(x => x.student_id == c.student_id
+                                && x.class_dtl_id == notification.BranchCourse.course_dtl_id && x.class_dtl_id == z.StandardID).FirstOrDefault();
+                                if (st != null)
+                                {
+                                    sendNotification(c.fcm_token, notification.NotificationMessage, notification.Notification_Date.ToString("dd/MM/yyyy"));
+                                }
+                            }
                         }
                     }
-                    
+                    //if (student?.Count > 0 ) {
+                    //    foreach (var i in student)
+                    //    {
+
+                    //    }
+                    //}
+
                 }
 
-                
+
 
             }
             catch (Exception ex)
@@ -347,7 +421,7 @@ namespace Ashirvad.Repo.Services.Area.Notification
             }
         }
 
-        public void sendNotification(string RegId,string msg,string datetext)
+        public void sendNotification(string RegId, string msg, string datetext)
         {
             string ApplicationID, SENDER_ID, message, value;
             try
@@ -358,7 +432,7 @@ namespace Ashirvad.Repo.Services.Area.Notification
                     SENDER_ID = "903914049924";
 
                     value = "Notification";  //title
-                    message = "Date- "+datetext+"\n"+ msg; //message
+                    message = "Date- " + datetext + "\n" + msg; //message
 
                     string images = "";
                     WebRequest tRequest;
@@ -384,14 +458,64 @@ namespace Ashirvad.Repo.Services.Area.Notification
                     //tResponse.Close();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 
             }
-        
+
         }
 
+        public async Task<bool> NotificationStandardMaintenance(NotificationEntity notificationInfo)
+        {
+            bool isSuccess = false;
+            List<string> da = new List<string>();
+            foreach (var z in notificationInfo.NotificationType)
+            {
+                da.Add(z.TypeID.ToString());
+            }
+            if (da?.Count == 1 && da.Contains("1"))
+            {
+                var data = (from noti in this.context.NOTIFICATION_STD_MASTER
+                            where noti.notif_id == notificationInfo.NotificationID
+                            select noti).ToList();
+                if (data?.Count > 0)
+                {
+                    this.context.NOTIFICATION_STD_MASTER.RemoveRange(data);
+                    this.context.SaveChanges();
+                }
+            }
+            else
+            {
+                var data = (from lib in this.context.NOTIFICATION_STD_MASTER
+                            where lib.notif_id == notificationInfo.NotificationID
+                            select lib).ToList();
+                if (data?.Count > 0)
+                {
+                    this.context.NOTIFICATION_STD_MASTER.RemoveRange(data);
+                }
+                foreach (var item in notificationInfo.Standardlist)
+                {
+                    if (item != null)
+                    {
+                        NOTIFICATION_STD_MASTER library = null;
+                        List<NOTIFICATION_STD_MASTER> notificationlist = new List<NOTIFICATION_STD_MASTER>();
+                        library = new NOTIFICATION_STD_MASTER()
+                        {
+                            notif_id = notificationInfo.NotificationID,
+                            course_dtl_id = notificationInfo.BranchCourse.course_dtl_id,
+                            class_dtl_id = item.StandardID
+                        };
+                        notificationlist.Add(library);
 
+                        this.context.NOTIFICATION_STD_MASTER.AddRange(notificationlist);
+                        this.context.SaveChanges();
+                        isSuccess = true;
+                    }
+                }
+
+            }
+            return isSuccess;
+        }
 
     }
 }
