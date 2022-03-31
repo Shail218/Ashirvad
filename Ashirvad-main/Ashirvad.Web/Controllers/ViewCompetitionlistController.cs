85f979d38e6e2198875ffee6b086ae8cefa26447
Smeet Patel <smeetpatel98@gmail.com>
2022-03-31 09:50:57 +0530

    Rank and View Competition- Smeet

---
+++ Ashirvad-main/Ashirvad.Web/Controllers/ViewCompetitionlistController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ashirvad.Data;
+using Ashirvad.Data.Model;
+
+namespace Ashirvad.Web.Controllers
+{
+    public class ViewCompetitionlistController : BaseController
+    {
+        // GET: ViewCompetitionlist
+        public ActionResult Index()
+        {
+            List<TestEntity> testEntity = new List<TestEntity>();
+            //TestMaintenanceModel branch = new TestMaintenanceModel();
+            //branch.TestInfo = new TestEntity();
+            //branch.TestInfo.test = new TestPaperEntity();
+            //branch.TestData = new List<TestEntity>();
+            return View(testEntity);
+        }
+    }
+}
